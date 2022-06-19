2f091aa2aa84919a8e77c94983c2c33264f8d500
Mviolets2i <mviolets2i@gmail.com>
2022-06-19 10:02:07 +0200

    Mise à jour du repo

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 import pdfplumber
+import os
 
 def extraction(path):
     """Extraction des données contenue dans une fiche de salaire education nationale au format PDF"""
     with pdfplumber.open(path) as pdf:
         data = pdf.pages[0].extract_table()
+    print("Accès au chemin :",path)
+    print("Données brutes :")
+    print(data)
     donnee = []
-    donnee.append({"Echelon":data[0][-11][-8:-6],"Indice":data[0][-11][-4:], "Corps":data[0][-11][-21:-15], "Grade":data[0][-11][-14:-12]})
-    for elem in data:
+    """for elem in data:
         try:
             if elem[0].isnumeric and len(elem[0]) == 6:
                 donnee.append({elem[0]:elem[2],payer(elem)[0]:payer(elem)[1]})
         except:
             None
-    return donnee
+    return donnee"""
 
 def main():
-    path = "D:\\Documents\\07 - Documents administratifs et Professionnels\\02 - Payes\\2022\\05 - Mai.pdf"
-    donnee = extraction(path)
-    calcul = calcul_compare(donnee)
-    affichage_extraction(donnee, calcul)
-    
+    paths = os.listdir('.\PDFs')
+    for path in paths:
+        extraction(os.getcwd()+'\\PDFs\\'+str(path))
+        #'.\PDFs\\'+str(path)
+        
 
 if __name__ == '__main__':
     main()
